d4e38ba70201ec59a310bfcc53384b348fb30f33
עופר מאנע - כללית <OferMa@clalit.org.il>
2026-08-30 12:44:33 +0300

    Updates, for saving code...

--- copy.new.Portion.from.Content.WinForms.v5.cmd
+++ copy.new.Portion.from.Content.WinForms.v5.cmd
@@ -37,7 +37,7 @@ enum LogLevel {
     Error
 }
 
-$Portion = 'Ki_Tavo'
+$Portion = 'Nitzavim_Vayeilech'
 
 $SourceDirectory = 'C:\Git\allproj\_OneFile\OpenServers_content'
 $M1T2Directory = 'C:\Git\!myRepos\mpages\m1t2'
