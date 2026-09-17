1e50e9edf7483b19e41121f1c2458473f963a894
StarCraft-il <links4me@gmail.com>
2026-09-17 17:45:30 +0300

    Updates, for saving code...

--- copy.new.Portion.from.Content.WinForms.v5.cmd
+++ copy.new.Portion.from.Content.WinForms.v5.cmd
@@ -37,7 +37,7 @@ enum LogLevel {
     Error
 }
 
-$Portion = "Ha'Azinu"
+$Portion = "Vezot_Haberakhah"
 
 $SourceDirectory = 'C:\Git\allproj\_OneFile\OpenServers_content'
 $M1T2Directory = 'C:\Git\!myRepos\mpages\m1t2'
